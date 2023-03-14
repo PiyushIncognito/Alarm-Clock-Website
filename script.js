dd41f23d1680351e4b02785b4efef6e52417f63a
PiyushIncognito <piyushchaudhari0514@gmail.com>
2023-03-14 22:01:41 +0530

    Script added

---
+++ script.js
@@ -0,0 +1,24 @@
+const selectMenu = document.querySelectorAll("select");
+
+for (let i = 12; i > 0; i--) {
+    i = i < 10 ? "0" + i : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[0].firstElementChild.insertAdjacentElement("afterend", option);
+    // console.log(i)
+}
+
+for (let i = 12; i > 0; i--) {
+    i = i < 10 ? "0" + i : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[0].firstElementChild.insertAdjacentElement("afterend", option);
+    // console.log(i)
+}
+
+for (let i = 12; i > 0; i--) {
+    i = i < 10 ? "0" + i : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[0].firstElementChild.insertAdjacentElement("afterend", option);
+    // console.log(i)
+}
+
+// console.log(selectMenu);
